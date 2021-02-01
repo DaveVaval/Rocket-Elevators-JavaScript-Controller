716ec5b3691fdf1895bea5a441941924854a6e5e
DaveGrim <DaveGrim@users.noreply.github.com>
2021-02-01 13:37:03 -0500

    console logging attributes to see if the values go to the right places

--- Residential_Controller.js
+++ Residential_Controller.js
@@ -1,20 +1,63 @@
 // Residential Controller
+let callbuttonID = 1
+let elevatorID = 1
+let floorRequestButtonID= 1
 
 // Step 1: Initializing classes
 
+// Column
 class Column {
     constructor(id,status,amountOfFloor,amountOfElevator){
         this.id = id;
         this.status = status
         this.amountOfElevator = amountOfElevator
         this.amountOfFloor = amountOfFloor
-        let callButtonList = []
-        let elevatorList = []
-    } createCallButtons () {
+        this.callButtonList = []
+        this.elevatorList = []
+    } 
+    createCallButtons () {
         for(i = 0; i < this.amountOfFloor; i++){
-            
+            let callButtons = new CallButton();
+            this.callButtonList.push(callButtons);
+            callbuttonID ++;
         }  
     }
 }
 
+
+// Elelvator
+class Elevator{
+    
+}
+
+
+
+
+// Call Button
+class CallButton{
+    constructor(id,status,floor,direction){
+        this.id = id
+        this.status = status
+        this.floor = floor
+        this.direction = direction
+    }
+}
+
+
+
+// Floor Request Button
+class FloorRequestButton{
+    
+}
+
+
+
+// Doors
+class Doors{
+    
+}
+
+
+
 // Scenarios
+let  residentialColum = new Column(1,1,10,2)
