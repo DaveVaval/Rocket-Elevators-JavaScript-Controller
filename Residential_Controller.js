4109ff5d9f6ce8c82e76c7294dc6ae30721ea3a0
DaveGrim <DaveGrim@users.noreply.github.com>
2021-02-01 12:39:59 -0500

    initializing classes

---
+++ Residential_Controller.js
@@ -0,0 +1,20 @@
+// Residential Controller
+
+// Step 1: Initializing classes
+
+class Column {
+    constructor(id,status,amountOfFloor,amountOfElevator){
+        this.id = id;
+        this.status = status
+        this.amountOfElevator = amountOfElevator
+        this.amountOfFloor = amountOfFloor
+        let callButtonList = []
+        let elevatorList = []
+    } createCallButtons () {
+        for(i = 0; i < this.amountOfFloor; i++){
+            
+        }  
+    }
+}
+
+// Scenarios
