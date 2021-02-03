d8119d7fc783d93ca49fdfb4626d34c49d10409e
DaveGrim <DaveGrim@users.noreply.github.com>
2021-02-03 14:46:47 -0500

    Last commit

--- Residential_Controller.js
+++ Residential_Controller.js
@@ -188,5 +188,68 @@ class Doors{
     }
 }
 
+//----------------------------------------------------------------------// testing //--------------------------------------------------------------------------------//
 
+function Scenario1(){
+    // Creating a new instance of Column
+    let  column1 = new Column(1, "online", 10, 2);
+    // Setting the base attributes for this scenario
+    column1.elevatorList[0].currentFloor = 2
+    column1.elevatorList[1].currentFloor = 6
+    // User at floor 3 wants to go up to floor 7
+    // Elevator A will be sent
+    let elevator1 = column1.requestElevator(3, "up")
+    elevator1.requestFloor(7)
+}
 
+function Scenario2(){
+    // Creating a new instance of Column
+    let column2 = new Column(2, "online", 10, 2)
+    // setting the base attributes for the start of this scenario
+    column2.elevatorList[0].currentFloor = 10
+    column2.elevatorList[1].currentFloor = 3
+    console.log("Elevator A current floor at the start:",column2.elevatorList[0].currentFloor)
+    console.log("Elevator B current floor at the start:",column2.elevatorList[1].currentFloor)
+    console.log("User is on floor 1 and presses on up...")
+    console.log("The column will send the Elevator B to the user...")
+    // User at floor 1 wants to go up to floor 6
+    // Elevator B will be sent
+    let elevator2 = column2.requestElevator(1, "up")
+    console.log("User presses on the floor button 6 inside of the elevator")
+    elevator2.requestFloor(6)
+    console.log("Elevator B reached floor:",column2.elevatorList[1].currentFloor)
+    console.log("User gets out...")
+    console.log("2 minutes later...")
+    console.log("User on floor 3 wants to go up to floor 5")
+    let elevator3 = column2.requestElevator(3, "up")
+    console.log("Elevator B is sent to floor:", column2.elevatorList[1].currentFloor)
+    console.log("User enters the elevator and presses on floor 5")
+    elevator3.requestFloor(5)
+    console.log("User reaches floor",column2.elevatorList[1].currentFloor,"and gets out")
+    console.log("Some time after...")
+    console.log("User on floor 9 wants to go up to floor 2")
+    let elevator4 = column2.requestElevator(9, "down")
+    console.log("Elevator A is sent to floor:", column2.elevatorList[0].currentFloor)
+    console.log("User enters the elevator and presses on floor 2")
+    elevator4.requestFloor(2)
+    console.log("User reaches floor",column2.elevatorList[0].currentFloor,"and gets out")
+}
+
+function Scenario3(){
+    // Creating a new instance of Column
+    let column3 = new Column(3, "online", 10, 2)
+    // setting the base attributes for the start of this scenario
+    column3.elevatorList[0].currentFloor = 10
+    column3.elevatorList[0].direction = "down" // elevator A is at floor 10 with direction down because it can only go down
+    column3.elevatorList[1].currentFloor = 3 // elevator B is at floor 3 and moving to floor 6
+    column3.elevatorList[1].requestFloor(6)
+    // User is at floor and wants to go down to floor 2
+    // Elevator A will be sent
+    let elevator5 = column3.requestElevator(3, "down")
+    elevator5.requestFloor(2)
+    // 5 minutes later, another user on floor 10 wants to go down to floor 3
+    // Elevator B will be sent
+    let elevator6 = column3.requestElevator(10, "down")
+    elevator6.requestFloor(3)
+}
+Scenario2();
