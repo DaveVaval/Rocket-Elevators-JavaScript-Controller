ec02bf338af71632c8ed4acb2216ea5e11a9281a
DaveGrim <DaveGrim@users.noreply.github.com>
2021-02-05 22:02:59 -0500

    Finale stage

--- README.md
+++ README.md
@@ -1,41 +1,15 @@
-# JavaScript Controller
+# Rocket Elevators JavaScript Controller 
 
-### This was one hell of a challenge!
+![alt text](https://github.com/DaveVaval/Rocket-Elevators-Ruby-Controller/blob/Main/img/R3.png)
 
-If you want to test the script you can paste Scenario2(); , and run the script in your terminal!
-Rocket Elevators TO THE MOON!
-```C#
-function Scenario2(){
-    // Creating a new instance of Column
-    let column2 = new Column(2, "online", 10, 2)
-    // setting the base attributes for the start of this scenario
-    column2.elevatorList[0].currentFloor = 10
-    column2.elevatorList[1].currentFloor = 3
-    console.log("Elevator A current floor at the start:",column2.elevatorList[0].currentFloor)
-    console.log("Elevator B current floor at the start:",column2.elevatorList[1].currentFloor)
-    console.log("User is on floor 1 and presses on up...")
-    console.log("The column will send the Elevator B to the user...")
-    // User at floor 1 wants to go up to floor 6
-    // Elevator B will be sent
-    let elevator2 = column2.requestElevator(1, "up")
-    console.log("User presses on the floor button 6 inside of the elevator")
-    elevator2.requestFloor(6)
-    console.log("Elevator B reached floor:",column2.elevatorList[1].currentFloor)
-    console.log("User gets out...")
-    console.log("2 minutes later...")
-    console.log("User on floor 3 wants to go up to floor 5")
-    let elevator3 = column2.requestElevator(3, "up")
-    console.log("Elevator B is sent to floor:", column2.elevatorList[1].currentFloor)
-    console.log("User enters the elevator and presses on floor 5")
-    elevator3.requestFloor(5)
-    console.log("User reaches floor",column2.elevatorList[1].currentFloor,"and gets out")
-    console.log("Some time after...")
-    console.log("User on floor 9 wants to go up to floor 2")
-    let elevator4 = column2.requestElevator(9, "down")
-    console.log("Elevator A is sent to floor:", column2.elevatorList[0].currentFloor)
-    console.log("User enters the elevator and presses on floor 2")
-    elevator4.requestFloor(2)
-    console.log("User reaches floor",column2.elevatorList[0].currentFloor,"and gets out")
-}
+Welcome to the JavaScript controller for Rocket Elevators! 
+
+- Javascript is a robust tool!
+
+An instance of column is created and when a en elevator is called, the column will send the best elevator to the user based on the different attributes of the elevators and the origin of the request.
+
+In order to see it in action, paste one of these scenarios at the bottom of the script and run it in your terminal:
+
 ```
-Now on to the next challenge!
+Scenario1() , Scenario2() , Scenario3()
+```
--- Residential_Controller.js
+++ Residential_Controller.js
@@ -1,33 +1,29 @@
-// Residential Controller
-let callbuttonID = 1
-let elevatorID = 1
-let floorRequestButtonID= 1
-
 // Column
 class Column {
-    constructor(_id,_status,_amountOfFloor,_amountOfElevator){
-        this.id = _id;
-        this.status = _status
-        this.amountOfElevator = _amountOfElevator
-        this.amountOfFloor = _amountOfFloor
-        this.callButtonList = []
-        this.elevatorList = []
+    constructor(id,status,amountOfFloors,amountOfElevators){
+        this.id = id;
+        this.status = status
+        this.amountOfElevators = amountOfElevators
+        this.amountOfFloors = amountOfFloors
+        this.callButtonsList = []
+        this.elevatorsList = []
         this.createCallButtons(); // Calling the functions to create the call buttons
         this.createElevators();   // as well as the elevators
     }
     // This creates all the call buttons on every floor
     createCallButtons(){
-        let numberOfCallButtons = this.amountOfFloor
+        let callbuttonID = 1
+        let numberOfCallButtons = this.amountOfFloors
         let buttonFloor = 1
         for(let i = 0; i < numberOfCallButtons; i++){
-            if(buttonFloor < this.amountOfFloor){
+            if(buttonFloor < this.amountOfFloors){
                 let callButton = new CallButton(callbuttonID,"off",buttonFloor,"up") // The first floor will only have a Up button
-                this.callButtonList.push(callButton);
+                this.callButtonsList.push(callButton);
                 callbuttonID ++;
             } 
             if(buttonFloor > 1){
                 let callButton = new CallButton(callbuttonID,"off",buttonFloor,"Down") // the last floor will only have a Down button
-                this.callButtonList.push(callButton);
+                this.callButtonsList.push(callButton);
                 callbuttonID ++;
             }
             buttonFloor ++;
@@ -35,9 +31,10 @@ class Column {
     }
     // this creates the elevators
     createElevators(){
-        for(let i = 0; i < this.amountOfElevator; i++){
-            let elevator = new Elevator(elevatorID,"idle",this.amountOfFloor,1)
-            this.elevatorList.push(elevator);
+        let elevatorID = 1
+        for(let i = 0; i < this.amountOfElevators; i++){
+            let elevator = new Elevator(elevatorID,"idle",this.amountOfFloors,1)
+            this.elevatorsList.push(elevator);
             elevatorID ++;
         }
     }
@@ -46,7 +43,7 @@ class Column {
         let elevator = this.findElevator(requestedFloor, direction);
         elevator.floorRequestList.push(requestedFloor);
         elevator.move();
-        elevator.openDoors();
+        elevator.openDoor();
         return elevator;
     }
     // this will find the best elevator depending of the score it recieves
@@ -56,7 +53,7 @@ class Column {
             bestScore: 5,
             referenceGap: Infinity
         }
-        this.elevatorList.forEach(elevator => {
+        this.elevatorsList.forEach(elevator => {
             if(requestedFloor == elevator.currentFloor && elevator.status == "idle" && requestedDirection == elevator.direction){
                 elevatorInfo = this.checkElevator(1, elevator, elevatorInfo, requestedFloor)
             }
@@ -96,33 +93,34 @@ class Column {
 
 // Elelvator
 class Elevator{
-    constructor(_id, _status, _amountOfFloor, _currentFloor){
-        this.id = _id
-        this.status = _status
-        this.amountOfFloor = _amountOfFloor
+    constructor(id, status, amountOfFloors, _currentFloor){
+        this.id = id
+        this.status = status
+        this.amountOfFloors = amountOfFloors
         this.direction = null
         this.currentFloor = _currentFloor
-        this.doors = new Door(_id, "closed") 
-        this.floorRequestButtonList = []
+        this.door = new Door(id, "closed") 
+        this.floorRequestButtonsList = []
         this.floorRequestList = []
         this.createFloorRequestButtons();
     }
     // method that creates the floor request buttons for each instance of elevator
     createFloorRequestButtons(){
+        let floorRequestButtonID= 1
         let floorNumber = 1
-        for(let i = 0; i < this.amountOfFloor; i++){
+        for(let i = 0; i < this.amountOfFloors; i++){
             let floorButton = new FloorRequestButton(floorRequestButtonID, "off", floorNumber)
-            this.floorRequestButtonList.push(floorButton);
+            this.floorRequestButtonsList.push(floorButton);
             floorNumber ++;
             floorRequestButtonID ++;
         }
     }
     // This is the main method that will opperate the Elevator once it recieves a floor
-    requestFloor(floor){
-        this.floorRequestList.push(floor)
+    requestFloor(requestedFloor){
+        this.floorRequestList.push(requestedFloor)
         this.sortFloorRequestList();
         this.move();
-        this.openDoors();
+        this.openDoor();
     }
     // This method will make the elevator move to the value of the first index of floor request list
     move(){ 
@@ -154,37 +152,37 @@ class Elevator{
             this.floorRequestList.sort(function(a,b){return b-a});
         }
     }
-    // the basic doors opening method
-    openDoors(){
-        this.doors.status = "open" 
-        this.doors.status = "closed"
+    // the basic door opening method
+    openDoor(){
+        this.door.status = "open" 
+        this.door.status = "closed"
     }
 }
 
 // Call Button
 class CallButton{
-    constructor(_id,_status,_floor,_direction){
-        this.id = _id
-        this.status = _status
-        this.floor = _floor
-        this.direction = _direction
+    constructor(id,status,floor,direction){
+        this.id = id
+        this.status = status
+        this.floor = floor
+        this.direction = direction
     }
 }
 
 // Floor Request Button
 class FloorRequestButton{
-    constructor(_id,_status,_floor){
-        this.id = _id
-        this.status = _status
-        this.floor = _floor
+    constructor(id,status,floor){
+        this.id = id
+        this.status = status
+        this.floor = floor
     }
 }
 
 // Door
 class Door{
-    constructor(_id,_status){
-        this.id = _id
-        this.status = _status
+    constructor(id,status){
+        this.id = id
+        this.status = status
     }
 }
 
@@ -194,63 +192,75 @@ function Scenario1(){
     // Creating a new instance of Column
     let  column1 = new Column(1, "online", 10, 2);
     // Setting the base attributes for this scenario
-    column1.elevatorList[0].currentFloor = 2
-    column1.elevatorList[1].currentFloor = 6
-    // User at floor 3 wants to go up to floor 7
-    // Elevator A will be sent
+    column1.elevatorsList[0].currentFloor = 2
+    column1.elevatorsList[1].currentFloor = 6
+    console.log("User at floor 3 wants to go up to floor 7")
     let elevator1 = column1.requestElevator(3, "up")
+    console.log("Elevator A will be sent to floor:",column1.elevatorsList[0].currentFloor)
     elevator1.requestFloor(7)
+    console.log("...")
+    console.log("User reaches floor",column1.elevatorsList[0].currentFloor,"and gets out")
 }
 
 function Scenario2(){
     // Creating a new instance of Column
     let column2 = new Column(2, "online", 10, 2)
     // setting the base attributes for the start of this scenario
-    column2.elevatorList[0].currentFloor = 10
-    column2.elevatorList[1].currentFloor = 3
-    console.log("Elevator A current floor at the start:",column2.elevatorList[0].currentFloor)
-    console.log("Elevator B current floor at the start:",column2.elevatorList[1].currentFloor)
+    column2.elevatorsList[0].currentFloor = 10
+    column2.elevatorsList[1].currentFloor = 3
     console.log("User is on floor 1 and presses on up...")
-    console.log("The column will send the Elevator B to the user...")
     // User at floor 1 wants to go up to floor 6
     // Elevator B will be sent
     let elevator2 = column2.requestElevator(1, "up")
+    console.log("The column will send the Elevator B to floor:", column2.elevatorsList[1].currentFloor)
     console.log("User presses on the floor button 6 inside of the elevator")
     elevator2.requestFloor(6)
-    console.log("Elevator B reached floor:",column2.elevatorList[1].currentFloor)
+    console.log("Elevator B reached floor:",column2.elevatorsList[1].currentFloor)
     console.log("User gets out...")
+    console.log("")
     console.log("2 minutes later...")
+    console.log("")
     console.log("User on floor 3 wants to go up to floor 5")
     let elevator3 = column2.requestElevator(3, "up")
-    console.log("Elevator B is sent to floor:", column2.elevatorList[1].currentFloor)
+    console.log("Elevator B is sent to floor:", column2.elevatorsList[1].currentFloor)
     console.log("User enters the elevator and presses on floor 5")
     elevator3.requestFloor(5)
-    console.log("User reaches floor",column2.elevatorList[1].currentFloor,"and gets out")
+    console.log("User reaches floor",column2.elevatorsList[1].currentFloor,"and gets out")
+    console.log("")
     console.log("Some time after...")
+    console.log("")
     console.log("User on floor 9 wants to go up to floor 2")
     let elevator4 = column2.requestElevator(9, "down")
-    console.log("Elevator A is sent to floor:", column2.elevatorList[0].currentFloor)
+    console.log("Elevator A is sent to floor:", column2.elevatorsList[0].currentFloor)
     console.log("User enters the elevator and presses on floor 2")
     elevator4.requestFloor(2)
-    console.log("User reaches floor",column2.elevatorList[0].currentFloor,"and gets out")
+    console.log("User reaches floor",column2.elevatorsList[0].currentFloor,"and gets out")
 }
 
 function Scenario3(){
     // Creating a new instance of Column
     let column3 = new Column(3, "online", 10, 2)
     // setting the base attributes for the start of this scenario
-    column3.elevatorList[0].currentFloor = 10
-    column3.elevatorList[0].direction = "down" // elevator A is at floor 10 with direction down because it can only go down
-    column3.elevatorList[1].currentFloor = 3 // elevator B is at floor 3 and moving to floor 6
-    column3.elevatorList[1].requestFloor(6)
-    // User is at floor and wants to go down to floor 2
-    // Elevator A will be sent
+    column3.elevatorsList[0].currentFloor = 10
+    column3.elevatorsList[0].direction = "down" // elevator A is at floor 10 with direction down because it can only go down
+    column3.elevatorsList[1].currentFloor = 3 // elevator B is at floor 3 and moving to floor 6
+    console.log("User is on floor 3 and wants to go up to floor 2")
+    console.log("Elevator A is on floor 10 and Elevator B is currently moving from floor 3 to 6")
+    column3.elevatorsList[1].requestFloor(6)
     let elevator5 = column3.requestElevator(3, "down")
+    console.log("Elevator A is sent to floor:", column3.elevatorsList[0].currentFloor)
+    console.log("User enters the elevator and presses on floor 2")
     elevator5.requestFloor(2)
-    // 5 minutes later, another user on floor 10 wants to go down to floor 3
-    // Elevator B will be sent
+    console.log("")
+    console.log("5 minutes later...")
+    console.log("")
+    console.log("Another user is on floor 10 and wants to go down to floor 3")
     let elevator6 = column3.requestElevator(10, "down")
+    console.log("Elevator B is sent to floor:", column3.elevatorsList[1].currentFloor)
+    console.log("User enters the elevator and presses on floor 3")
     elevator6.requestFloor(3)
+    console.log("...")
+    console.log("User reaches floor",column3.elevatorsList[1].currentFloor,"and gets out")
 }
 
 module.exports = {Column, Elevator, CallButton, FloorRequestButton, Door}
